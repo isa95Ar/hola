5aab0ff05249d315eb70065bca4346f43453bb4b
isaias <isaarg2312@gmail.com>
2026-04-06 16:15:10 -0300

    Adding new page

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         eje: resolve(__dirname, 'eje.html'),
+        objetivo: resolve(__dirname, 'objetivo.html'),
       },
     },
   },
